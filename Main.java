dc4703a0da1e597fb79b2cd9940a7b3a86fca713
CrashKillerYT <crashkillerlps.de@gmail.com>
2018-02-03 23:57:07 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
-package de.gamingchain;
+package net.viewdns.applecraft;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -50,6 +51,7 @@ public class Main extends JavaPlugin implements Listener{
 		Player p = e.getPlayer();
 		
 		if (p.isOp() || p.hasPermission("*")) {
+			p.playSound(p.getLocation(), Sound.ENDERDRAGON_GROWL, 3, 1);
 			p.sendMessage("");
 			p.sendMessage("");
 			p.sendMessage("");
@@ -59,13 +61,15 @@ public class Main extends JavaPlugin implements Listener{
 			p.sendMessage("§a[TimeServer]");
 			p.sendMessage("§6Version: alpha-1.2");
 			p.sendMessage("§6Author: CrashKillerYT");
-			p.sendMessage("§6Website:http://applecraft.viewdns.net/ & http://www.gamingchain.de/");
+			p.sendMessage("§6Website:http://applecraft.viewdns.net/");
 			p.sendMessage("§6If you have some problems, inform me at:");
 			p.sendMessage("§6crashkillerlps.de@gmail.com");
 			p.sendMessage("§cATTENTION : Do first /gamerule dodaylightcycle false !!!");
+			p.sendMessage("§cYou can not type this command: /time set <time> !");
 			p.sendMessage("§bThis plugin is an OpenSource project !");
 			p.sendMessage("§bIt means, that you have all rights reserved !");
 			p.sendMessage("§bYou have all rights reserved, except of the MIT License !");
+			p.sendMessage("§k...§r§b We are now in the beta ! §r§k...");
 			p.sendMessage("§6I wish you much fun with that. Your Faithful CrashKillerYT ! :)");
 			p.sendMessage("__________________________________________________");
 			p.sendMessage("");
@@ -75,6 +79,6 @@ public class Main extends JavaPlugin implements Listener{
 			p.sendMessage("");
 		}
 		
-	}
-	
-}
+			}	
+		
+				}
