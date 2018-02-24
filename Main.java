c570066910050670c49b56b047f705e9be606e4b
CrashKillerYT <crashkillerlps.de@gmail.com>
2018-02-24 19:16:28 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,14 +13,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import net.gravitydevelopment.updater.Updater;
-import net.gravitydevelopment.updater.Updater.UpdateType;
 
 public class Main extends JavaPlugin implements CommandExecutor{
 	
+	@SuppressWarnings("unused")
 	@Override
 	public void onEnable() {
-		@SuppressWarnings("unused")
-		Updater updater = new Updater(this, 286270, getFile(), UpdateType.DEFAULT, true);
+		Updater updater = new Updater(this, 286270, getFile(), Updater.UpdateType.DEFAULT, true);
 		getCommand("info").setExecutor(this);
 		System.out.println("§2Time Server started");
 		
@@ -54,58 +53,96 @@ public class Main extends JavaPlugin implements CommandExecutor{
 		Player p = (Player)sender;
 		
 		if(cmd.getName().equalsIgnoreCase("info")) {
-			p.sendMessage("");
-			p.sendMessage("");
-			p.sendMessage("");
-			p.sendMessage("");
-			p.sendMessage("");
-			p.sendMessage("__________________________________________________");
-			p.sendMessage("§a" + getDescription().getName());
-			p.sendMessage("§6Version: " + getDescription().getVersion());
-			p.sendMessage("§6Author: " + getDescription().getAuthors());
-			p.sendMessage("§6Website: " + getDescription().getWebsite());
-			p.sendMessage("§6If you have some problems, inform me at: ");
-			p.sendMessage("§6crashkillerlps.de@gmail.com");
-			p.sendMessage("§cATTENTION: Do first /gamerule dodaylightcycle false !!!");
-			p.sendMessage("§cYou can not type this command: /time set <time> !");
-			p.sendMessage("§bThis plugin is an OpenSource project !");
-			p.sendMessage("§bIt means, that you have all rights reserved !");
-			p.sendMessage("§bYou have all rights reserved, except of the MIT License !");
-			p.sendMessage("§k...§r§b We are now released ! §r§k...");
-			p.sendMessage("§6We will support the latest versions soon, too !");
-			p.sendMessage("§6I wish you much fun with that. Yours Faithfuly CrashKillerYT ! :)");
-			p.sendMessage("__________________________________________________");
-			p.sendMessage("");
-			p.sendMessage("");
-			p.sendMessage("");
-			p.sendMessage("");
-			p.sendMessage("");
+			if(p.hasPermission("mcrealtime.info")) {
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("__________________________________________________");
+				p.sendMessage("§a" + getDescription().getName());
+				p.sendMessage("");
+				p.sendMessage("§6Version: " + getDescription().getVersion());
+				p.sendMessage("§6Author: " + getDescription().getAuthors());
+				p.sendMessage("§6Website: " + getDescription().getWebsite());
+				p.sendMessage("§6If you have some problems, inform me at: ");
+				p.sendMessage("§6crashkillerlps.de@gmail.com");
+				p.sendMessage("§cATTENTION: Do first /gamerule dodaylightcycle false !!!");
+				p.sendMessage("§cYou can not type this command: /time set <time> !");
+				p.sendMessage("§bThis plugin is an OpenSource project !");
+				p.sendMessage("§bIt means, that you have all rights reserved !");
+				p.sendMessage("§bYou have all rights reserved, except of the MIT License !");
+				p.sendMessage("§k...§r§b We are now released ! §r§k...");
+				p.sendMessage("§6We will support the latest versions soon, too !");
+				p.sendMessage("§6I wish you much fun with that. Yours Faithfuly CrashKillerYT ! :)");
+				p.sendMessage("__________________________________________________");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+			}else {
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("__________________________________________________");
+				p.sendMessage("§a" + getDescription().getName());
+				p.sendMessage("");
+				p.sendMessage("§cYou do not have permissions to perform this command !");
+				p.sendMessage("__________________________________________________");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+			}
 		}
 		
 		if(cmd.getName().equalsIgnoreCase("contact")) {
-			p.sendMessage("");
-			p.sendMessage("");
-			p.sendMessage("");
-			p.sendMessage("");
-			p.sendMessage("");
-			p.sendMessage("__________________________________________________");
-			p.sendMessage("§a" + getDescription().getName());
-			p.sendMessage("§6You have some problems or you want post a feedback ?");
-			p.sendMessage("§6No problem ! You could me send your feedback on the chat of:");
-			p.sendMessage("§6https://dev.bukkit.org/projects/mcrealtime");
-			p.sendMessage("");
-			p.sendMessage("§6If you have some problems, you can contact me under:");
-			p.sendMessage("§6E-Mail: crashkillerlps.de@gmail.com");
-			p.sendMessage("§bSometimes on my teamspeak 3: applecraft.viewdns.net");
-			p.sendMessage("§aTelegram: CrashKillerYT");
-			p.sendMessage("§6Thanks for understanding !");
-			p.sendMessage("§6See ya around :)");
-			p.sendMessage("__________________________________________________");
-			p.sendMessage("");
-			p.sendMessage("");
-			p.sendMessage("");
-			p.sendMessage("");
-			p.sendMessage("");
+			if(p.hasPermission("mcrealtime.contact")) {
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("__________________________________________________");
+				p.sendMessage("§a" + getDescription().getName());
+				p.sendMessage("");
+				p.sendMessage("§6You have some problems or you want post a feedback ?");
+				p.sendMessage("§6No problem ! You could me send your feedback on the chat of:");
+				p.sendMessage("§6https://dev.bukkit.org/projects/mcrealtime");
+				p.sendMessage("");
+				p.sendMessage("§6If you have some problems, you can contact me under:");
+				p.sendMessage("§6E-Mail: crashkillerlps.de@gmail.com");
+				p.sendMessage("§bSometimes on my teamspeak 3: applecraft.viewdns.net");
+				p.sendMessage("§aTelegram: CrashKillerYT");
+				p.sendMessage("§6Thanks for understanding !");
+				p.sendMessage("§6See ya around :)");
+				p.sendMessage("__________________________________________________");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+			}else {
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("__________________________________________________");
+				p.sendMessage("§a" + getDescription().getName());
+				p.sendMessage("");
+				p.sendMessage("§cYou do not have permissions to perform this command !");
+				p.sendMessage("__________________________________________________");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+				p.sendMessage("");
+			}
 		}
 		
 		return true;
