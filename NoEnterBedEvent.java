b5bb4f156178908c06d26398e483c5b9c06a10f1
CrashKillerYT <crashkillerlps.de@gmail.com>
2019-12-15 21:39:27 +0100

    Create NoEnterBedEvent.java

---
+++ NoEnterBedEvent.java
@@ -0,0 +1,33 @@
+package net.viewdns.applecraft;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
+
+public class NoEnterBedEvent implements Listener {
+	
+	@EventHandler(priority=EventPriority.HIGHEST)
+	public void onEnterBed(PlayerBedEnterEvent e) {
+		
+			Player p = e.getPlayer();
+			e.setCancelled(true);
+			p.sendMessage("");
+			p.sendMessage("");
+			p.sendMessage("");
+			p.sendMessage("");
+			p.sendMessage("");
+			p.sendMessage("__________________________________________________");
+			p.sendMessage("§a♦ MCRealTime §2♦");
+			p.sendMessage("");
+			p.sendMessage("§cYou can't sleep while real time simulation. Comming maybe soon !");
+			p.sendMessage("__________________________________________________");
+			p.sendMessage("");
+			p.sendMessage("");
+			p.sendMessage("");
+			p.sendMessage("");
+		
+	}
+
+}
